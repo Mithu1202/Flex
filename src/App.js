dfe64c750d7bac52dc4243f83839534e8165c3cd
mithuran2002 <mithuran64@gmail.com>
2024-12-02 11:33:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,120 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="navbar">
+        <div className="logo">Logo</div>
+        <div className="menu">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#services">Services</a>
+          <a href="#contact">Contact</a>
+        </div>
+      </div>
+
+      <div className="content">
+        <div className="sidebar">
+          <p>Item 1</p>
+          <p>Item 2</p>
+          <p>Item 3</p>
+          <p>Item 1</p>
+          <p>Item 2</p>
+          <p>Item 3</p>
+          <p>Item 1</p>
+          <p>Item 2</p>
+          <p>Item 3</p>
+          <p>Item 1</p>
+          <p>Item 2</p>
+          <p>Item 3</p>
+          <p>Item 1</p>
+          <p>Item 2</p>
+          <p>Item 3</p>
+          <p>Item 1</p>
+          <p>Item 2</p>
+          <p>Item 3</p>
+          <p>32321321</p>
+        </div>
+
+        <div className="main">
+          <h1>Welcome to the Main Page</h1>
+          <p>ajhdagjhajhajbdabcibiuiunacn asdonajnudn aoifco</p>
+          <div style={{ height: "1000px" }}>
+            <p>n
+            Why do we use it?
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+
+            Where does it come from?
+            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+            The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+
+            Where can I get some?
+            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.
+             All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. 
+             It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, 
+             to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+
+            5
+              paragraphs
+              words
+              bytes
+              lists
+              Start with 'Lorem
+            ipsum dolor sit amet...'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+
+          Section 1.10.33 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
+          "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+
+          1914 translation by H. Rackham
+          "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."</p>
+          
+
+          <div class="card-container">
+
+          <div class="card">
+            <img src="https://posterjack.ca/cdn/shop/articles/Tips_for_Taking_Photos_at_the_Beach_55dd7d25-11df-4acf-844f-a5b4ebeff4df.jpg?v=1563409972&width=1500"class="card-image" />
+            <div class="card-content">
+              <h3 class="card-title">kldfffffffffffffmvvvvvvvvv</h3>
+              <p class="card-description">
+                vndkjdjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjkdjjdjjjjjjjjjjjjjjjjjjjjjjjdddddddddddddddddd
+              </p>
+              <button class="card-button">here</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/011/991/883/small/the-concept-was-discouraged-and-hopeless-a-lonely-and-heartbreaking-concept-free-photo.jpg"class="card-image" />
+            <div class="card-content">
+              <h3 class="card-title">igfdbuvshdjhfhbsjhcbbuhdfbudbjhsbjhdbf</h3>
+              <p class="card-description">
+                kcjnvbsjhbfshjvbkiu iiusifubihs bsb ibih ishifiskbigfib susb i
+              </p>
+              <button class="card-button">here</button>
+            </div>
+          </div>
+
+          <div class="card">
+            <img src="https://static.vecteezy.com/system/resources/previews/031/588/507/non_2x/loneliness-and-heartbreak-a-poignant-exploration-of-emotional-vulnerability-and-solitude-ai-generated-photo.jpg" class="card-image" />
+            <div class="card-content">
+              <h3 class="card-title">jhbsgcvkh hsiu  diuhsifi s</h3>
+              <p class="card-description">
+                kjsbyu dgdkshkdu kshk hskhd kh sfdjnkbjb jfdbjkfdj kjnkdjf kjndjk fh djnkj dkjf kjd fkd kkjdhkfh kdjkf kjdfk khd iush kjnisuhdiu skhdu skhdkshkd kjhsdksbjdjhbjhdbs jhsbdjh jsb,hs h fhsh bjshdkch ks hkdks b jkajsjdfhskfjk
+              </p>
+              <button class="card-button">here</button>
+            </div>
+          </div>
+          </div>
+        </div>
+        </div>
+      </div>
+      <div className="footer">
+        <p>this is footer</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Grid.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Grid.css'
+
+function Grid() {
+  return (
+    <div className='outerbox'>
+
+        <div className='box'>
+            <div className='box1'></div>
+            <div className='box1'></div>
+            <div className='box1'></div>
+            <div className='box1'></div>
+        </div>
+
+        {/* <div className='container'>
+            <div className="grid-item">hi hello, this is grid</div>
+            <div className="grid-item">hi hello, this is grid</div>
+            <div className="grid-item">hi hello, this is grid</div>
+            <div className="grid-item">hi hello, this is grid</div>
+            <div className="grid-item">hi hello, this is grid</div>
+            <div className="grid-item">hi hello, this is grid</div>
+            <div className="grid-item">hi hello, this is grid</div>
+            <div className="grid-item">hi hello, this is grid</div>
+            <div className="grid-item">hi hello, this is grid</div>
+            <div className="grid-item">hi hello, this is grid</div>
+            <div className="grid-item">hi hello, this is grid</div>
+            <div className="grid-item">hi hello, this is grid</div>
+        </div> */}
+    </div>
+  )
+}
+
+export default Grid
